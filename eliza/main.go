9a5adec5b83dabb9a3c667a4f9d41e0c205330f5
host1812 <host1812@gmail.com>
2022-07-29 16:42:48 -0700

    finish with eliza

--- eliza/main.go
+++ eliza/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/host1812/gtutor/eliza/doctor"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	fmt.Println(doctor.Intro())
+	for {
+		fmt.Print("> ")
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		if userInput == "quit" {
+			break
+		}
+		response := doctor.Response(userInput)
+		fmt.Println(response)
+	}
 }
