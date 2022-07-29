1536ad164fad21ebcff2748d30406642fd2e0a98
host1812 <host1812@gmail.com>
2022-07-29 16:04:57 -0700

    finish with hello world

--- hello-world/main.go
+++ hello-world/main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	var simpleSay string
+	simpleSay = "Testing test"
+
+	anotherSay := "Another say"
+	sayHello(simpleSay)
+	sayHello(anotherSay)
+}
+
+func sayHello(what string) {
+	fmt.Println(what)
 }
